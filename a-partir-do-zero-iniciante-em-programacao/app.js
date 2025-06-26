64c92f15dfaec1bb9980620bdc9c248ca5a97e08
JotaP53 <martinsjp53@gmail.com>
2025-06-26 16:41:23 -0300

    update repo

---
+++ a-partir-do-zero-iniciante-em-programacao/app.js
@@ -0,0 +1,9 @@
+alert('Boas vindas ao jogo do número secreto.');
+let numeroSecreto = 5;
+let chute  = prompt('Escolha um número entre 1 e 10.');
+
+if (numeroSecreto == chute) {
+    console.log("Você acertou o número secreto (5).")
+} else {
+    alert("Você errou.")
+}
